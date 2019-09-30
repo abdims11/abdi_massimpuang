8115e69103ebe4ae0b64cdae85452bc69b474e0a
tenriajeng <ilhamtenriajeng03@gmail.com>
2019-10-01 02:44:28 +0800

    penambahan tugas 2 biodata mahasiswa

---
+++ src/tugas2_152101/Biodata.java
@@ -0,0 +1,24 @@
+package tugas2_152101;
+import java.util.Scanner;
+
+public class Biodata 
+{
+    public static void main(String[] args) 
+    {
+        String nama, nim, umur;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("BIODATA MAHASISWA\n");
+        
+        System.out.print("Masukkan Nama Anda \t: ");
+        nama = input.nextLine();
+        System.out.print("Masukkan Stambuk Anda \t: ");
+        nim = input.nextLine();
+        System.out.print("Masukkan Umur Anda \t: ");
+        umur = input.nextLine();
+        
+        System.out.println("");
+        System.out.println("Nim " + nim + " Dengan Nama " + nama + " Berumur " + umur + " Tahun");        
+    }
+}
