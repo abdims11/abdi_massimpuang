c37be101e67a89846406ddfd5e887c7d13ede831
fatah <fatah@DESKTOP-IG1H4EJ>
2019-10-30 23:50:41 +0800

    penambahan pekage tugas 6

---
+++ src/tugas6_162101/tugas6.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas6_162101;
+
+/**
+ *
+ * @author fatah
+ */
+public class tugas6 {
+    
+}
